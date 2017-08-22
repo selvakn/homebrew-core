cfd2910dd1d50dbcfa21a56e5c5b9c8a7617a19a
Selva <k.n.selvakumar@gmail.com>
2017-08-22 17:43:31 +0530

    Update p2p-port-forward formula v1.1.1

--- Formula/p2p-port-forward.rb
+++ Formula/p2p-port-forward.rb
@@ -2,9 +2,9 @@ class P2pPortForward < Formula
   desc "Command-line utility to forward ports using zerotier-sdk"
   homepage "https://github.com/selvakn/p2p-port-forward"
 
-  url "https://github.com/selvakn/p2p-port-forward/releases/download/v1.0.1/p2p-port-forward-darwin-10.11-amd64"
-  version "1.0.1"
-  sha256 "2a52ca89edf34a95ecc64b9f0425f61d92d71e0d9678a54e2fa3d874e677239a"
+  url "https://github.com/selvakn/p2p-port-forward/releases/download/v1.1.1/p2p-port-forward-darwin-10.11-amd64"
+  version "1.1.1"
+  sha256 "d3bcc94204cea863486d019bdf3d89ccda38ee95a1d7c48687e5410cc378363c"
 
   bottle :unneeded
 
